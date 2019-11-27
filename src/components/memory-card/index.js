af0d74159b8b0503e158d1d98e21cc40e4e943ac
gregolly <gregollyff@hotmail.com>
2019-11-26 22:09:57 -0400

    refatoração do codigo

--- src/components/memory-card/index.js
+++ src/components/memory-card/index.js
@@ -1,15 +1,42 @@
-
-createMemoryCard();
-createMemoryCard();
-createMemoryCard();
-createMemoryCard();
-createMemoryCard();
-createMemoryCard();
-createMemoryCard();
-createMemoryCard();
-createMemoryCardFront();
-createMemoryCardFront();
-
+const createMemoryCard = () => `
+  <article class="memory-card">
+      <img 
+        src='img/icon-collabcode.png' 
+        alt='gueio mascote da collabcode' 
+        class='icon'
+        />
+      </img>
+  </article>
+`;
 
 
-clicarCard();
+
+const createMemoryCardFront = () => `
+  <article class="memory-card -front">
+      <img 
+        src='img/icon-js.png' 
+        alt='gueio mascote da collabcode' 
+        class='icon'
+        />
+      </img>
+  </article>
+`;
+
+
+//onClick="handleClick()"
+//função que captura o click de varios itens
+function clicarCard(){
+
+  const cards = document.querySelectorAll('.memory-card');
+
+
+  function handleCard(event){
+    const target = event.currentTarget;
+    target.classList.toggle('teste');
+  }
+
+  cards.forEach((card) =>{
+    card.addEventListener('click', handleCard);
+  }); 
+
+}
--- src/pages/game/index.js
+++ src/pages/game/index.js
@@ -1,51 +1,19 @@
-function createMemoryCard(){
+const $root = document.querySelector('#root');
 
-  const $memoryCard = document.createElement("article");
-  
-  const $iconCollab = `<img 
-                    src='img/icon-collabcode.png' 
-                    alt='gueio mascote da collabcode' 
-                    class='icon'>
-                </img>`;
-  
-  $memoryCard.classList.add('memory-card');
-  $wrapCards.insertBefore($memoryCard, null);
-  $memoryCard.insertAdjacentHTML('afterbegin', $iconCollab);
-  
-}
+const $cardsWrapper = createCardsWrapper();
+const $memoryCard = createMemoryCard();
+const $memoryCardFront = createMemoryCardFront();
 
+$root.insertAdjacentElement('beforeend', $cardsWrapper);
 
-
-function createMemoryCardFront(){
-
-const $memoryCardFront = document.createElement("article");
-
-const $iconJs = `<img 
-                  src="img/icon-js.png" 
-                  alt="icone livro do livro javascript" 
-                  class="icon">`
-
-$memoryCardFront.classList.add('memory-card');
-$memoryCardFront.classList.add('-front'); 
-
-$wrapCards.insertBefore($memoryCardFront, null);
-
-$memoryCardFront.insertAdjacentHTML('afterbegin', $iconJs);
+$cardsWrapper.insertAdjacentHTML('beforeend', $memoryCard);
+$cardsWrapper.insertAdjacentHTML('beforeend', $memoryCard);
+$cardsWrapper.insertAdjacentHTML('beforeend', $memoryCard);
+$cardsWrapper.insertAdjacentHTML('beforeend', $memoryCard);
+$cardsWrapper.insertAdjacentHTML('beforeend', $memoryCard);
+$cardsWrapper.insertAdjacentHTML('beforeend', $memoryCard);
+$cardsWrapper.insertAdjacentHTML('beforeend', $memoryCardFront);
+$cardsWrapper.insertAdjacentHTML('beforeend', $memoryCardFront);
 
 
-}
-
-
-function clicarCard(){
-
-  const cards = document.querySelectorAll('.memory-card');
-
-
-  function handleCard(event){
-    console.log(event.currentTarget);
-  }
-
-  cards.forEach((card) =>{
-    card.addEventListener('click', handleCard);
-  }); 
-}
+clicarCard();
